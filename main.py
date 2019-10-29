9d340f332794efee460a27e480b5e473c50c0bc7
Jonathan Clark <jonathan84clark@yahoo.com>
2019-10-28 21:15:07 -0700

    Added support for driving the vehicle Fixed a number of minor issues with the webserver Added the camera streaming script

--- main.py
+++ main.py
@@ -16,11 +16,32 @@ import datetime
 import json
 import time
 from os.path import expanduser
+import RPi.GPIO as GPIO
 
 supported_files = {".html" : 'text/html', ".css" : 'text/css', "jpeg" : 'image/jpeg',
                    ".js" : 'text/javascript'}
 
 dataSet = {"mode" : 0.0 }
+
+# Setup the GPIO pins
+GPIO.setwarnings(False) # Disable unused warnings
+GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+GPIO.setup(2, GPIO.OUT) # Heater Pin
+GPIO.setup(3, GPIO.OUT) # Fan Pin
+GPIO.setup(4, GPIO.OUT) # AC Pin
+GPIO.setup(17, GPIO.OUT) # Main power line
+GPIO.setup(27, GPIO.OUT) # Main power line
+GPIO.setup(22, GPIO.OUT) # Main power line
+
+# Clear all relays
+GPIO.output(2, GPIO.LOW)
+GPIO.output(3, GPIO.LOW)
+GPIO.output(4, GPIO.LOW)
+GPIO.output(17, GPIO.LOW)
+GPIO.output(27, GPIO.HIGH)
+GPIO.output(22, GPIO.LOW)
+
+headlight_state = False
 
 class S(BaseHTTPRequestHandler):
     def _set_headers(self):
@@ -32,7 +53,7 @@ class S(BaseHTTPRequestHandler):
         if (self.path == "/"):
             self.path = "/index.html"
         
-        curdir = "C:\Users\jonat\Documents\GitHub\CameraRobot"
+        curdir = "/home/pi/CameraRobot"
         file_path = curdir + sep + self.path
         filename, file_extension = os.path.splitext(file_path)
         if (self.path == "/data.js"):
@@ -66,25 +87,60 @@ class S(BaseHTTPRequestHandler):
             if (len(key_value) == 2):
                 if (key_value[0] == "forward"):
                     self.change_movement(0)
-                elif (key_value[0] == "reverse"):
+                elif (key_value[0] == "stopforward"):
                     self.change_movement(1)
-                elif (key_value[0] == "stop"):
+                elif (key_value[0] == "reverse"):
                     self.change_movement(2)
-                elif (key_value[0] == "left"):
+                elif (key_value[0] == "stopreverse"):
                     self.change_movement(3)
-                elif (key_value[0] == "right"):
+                elif (key_value[0] == "center"):
                     self.change_movement(4)
-                elif (key_value[0] == "stopleft"):
+                elif (key_value[0] == "left"):
                     self.change_movement(5)
-                elif (key_value[0] == "stopright"):
+                elif (key_value[0] == "right"):
                     self.change_movement(6)
+                elif (key_value[0] == "stopleft"):
+                    self.change_movement(7)
+                elif (key_value[0] == "stopright"):
+                    self.change_movement(8)
+                elif (key_value[0] == "toggle_lights"):
+                    self.headlights()
             else:
                 pass#write_log("SERVER", 1, "ERROR: Invalid command format: " + post_str)
             
         self._set_headers()
         self.wfile.write("<html><body><h1>" + output + "</h1></body></html>")
 
+    def headlights(self):
+        global headlight_state
+        if headlight_state:
+            GPIO.output(27, GPIO.HIGH)
+            GPIO.output(22, GPIO.LOW)
+            headlight_state = False
+        else:
+            GPIO.output(27, GPIO.LOW)
+            GPIO.output(22, GPIO.HIGH)
+            headlight_state = True
+
     def change_movement(self, input):
+        if input == 0:
+            GPIO.output(2, GPIO.HIGH)
+            GPIO.output(3, GPIO.LOW)
+        elif input == 2:
+            GPIO.output(2, GPIO.LOW)
+            GPIO.output(3, GPIO.HIGH)
+        elif input == 1 or input == 3:
+            GPIO.output(2, GPIO.LOW)
+            GPIO.output(3, GPIO.LOW)
+        elif input == 4:
+            GPIO.output(4, GPIO.LOW)
+            GPIO.output(17, GPIO.LOW)
+        elif input == 5:
+            GPIO.output(4, GPIO.HIGH)
+            GPIO.output(17, GPIO.LOW)
+        elif input == 6:
+            GPIO.output(4, GPIO.LOW)
+            GPIO.output(17, GPIO.HIGH)
         print("Got movement post: " +  str(input))
 
 def runServer(server_class=HTTPServer, handler_class=S, port=5000):
@@ -108,5 +164,5 @@ if __name__ == "__main__":
     thread.start()
 
     while (True):
-        pass
+        time.sleep(1)
         
