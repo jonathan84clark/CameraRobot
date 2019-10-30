2973bc3e01cae79f36f3cbfbb1dd7391fa9d181a
Jonathan Clark <jonathan84clark@yahoo.com>
2019-10-29 20:11:00 -0700

    Cleaned up the code and added enums to make it more clear what is going on

--- main.py
+++ main.py
@@ -17,31 +17,47 @@ import json
 import time
 from os.path import expanduser
 import RPi.GPIO as GPIO
+from enum import Enum
 
 supported_files = {".html" : 'text/html', ".css" : 'text/css', "jpeg" : 'image/jpeg',
                    ".js" : 'text/javascript'}
 
 dataSet = {"mode" : 0.0 }
 
+FORWARD_A = 2
+FORWARD_B = 3
+STEERING_A = 4
+STEERING_B = 17
+HEADLIGHTS_1 = 27
+HEADLIGHTS_2 = 22
+
 # Setup the GPIO pins
 GPIO.setwarnings(False) # Disable unused warnings
 GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
-GPIO.setup(2, GPIO.OUT) # Heater Pin
-GPIO.setup(3, GPIO.OUT) # Fan Pin
-GPIO.setup(4, GPIO.OUT) # AC Pin
-GPIO.setup(17, GPIO.OUT) # Main power line
-GPIO.setup(27, GPIO.OUT) # Main power line
-GPIO.setup(22, GPIO.OUT) # Main power line
+GPIO.setup(FORWARD_A, GPIO.OUT) # Heater Pin
+GPIO.setup(FORWARD_B, GPIO.OUT) # Fan Pin
+GPIO.setup(STEERING_A, GPIO.OUT) # AC Pin
+GPIO.setup(STEERING_B, GPIO.OUT) # Main power line
+GPIO.setup(HEADLIGHTS_1, GPIO.OUT) # Main power line
+GPIO.setup(HEADLIGHTS_2, GPIO.OUT) # Main power line
 
 # Clear all relays
-GPIO.output(2, GPIO.LOW)
-GPIO.output(3, GPIO.LOW)
-GPIO.output(4, GPIO.LOW)
-GPIO.output(17, GPIO.LOW)
-GPIO.output(27, GPIO.HIGH)
-GPIO.output(22, GPIO.LOW)
+GPIO.output(FORWARD_A, GPIO.LOW)
+GPIO.output(FORWARD_B, GPIO.LOW)
+GPIO.output(STEERING_A, GPIO.LOW)
+GPIO.output(STEERING_B, GPIO.LOW)
+GPIO.output(HEADLIGHTS_1, GPIO.HIGH)
+GPIO.output(HEADLIGHTS_2, GPIO.LOW)
 
 headlight_state = False
+
+class Direction(Enum):
+    FORWARD = 1
+    REVERSE = 2
+    STOP = 3
+    LEFT = 4
+    RIGHT = 5
+    CENTER = 6
 
 class S(BaseHTTPRequestHandler):
     def _set_headers(self):
@@ -86,19 +102,19 @@ class S(BaseHTTPRequestHandler):
             key_value = post_str.split("=")
             if (len(key_value) == 2):
                 if (key_value[0] == "forward"):
-                    self.change_movement(0)
+                    self.change_movement(Direction.FORWARD)
                 elif (key_value[0] == "stopforward"):
-                    self.change_movement(1)
+                    self.change_movement(Direction.STOP)
                 elif (key_value[0] == "reverse"):
-                    self.change_movement(2)
+                    self.change_movement(Direction.REVERSE)
                 elif (key_value[0] == "stopreverse"):
-                    self.change_movement(3)
+                    self.change_movement(Direction.STOP)
                 elif (key_value[0] == "center"):
-                    self.change_movement(4)
+                    self.change_movement(Direction.CENTER)
                 elif (key_value[0] == "left"):
-                    self.change_movement(5)
+                    self.change_movement(Direction.LEFT)
                 elif (key_value[0] == "right"):
-                    self.change_movement(6)
+                    self.change_movement(Direction.RIGHT)
                 elif (key_value[0] == "stopleft"):
                     self.change_movement(7)
                 elif (key_value[0] == "stopright"):
@@ -114,33 +130,33 @@ class S(BaseHTTPRequestHandler):
     def headlights(self):
         global headlight_state
         if headlight_state:
-            GPIO.output(27, GPIO.HIGH)
-            GPIO.output(22, GPIO.LOW)
+            GPIO.output(HEADLIGHTS_1, GPIO.HIGH)
+            GPIO.output(HEADLIGHTS_2, GPIO.LOW)
             headlight_state = False
         else:
-            GPIO.output(27, GPIO.LOW)
-            GPIO.output(22, GPIO.HIGH)
+            GPIO.output(HEADLIGHTS_1, GPIO.LOW)
+            GPIO.output(HEADLIGHTS_2, GPIO.HIGH)
             headlight_state = True
 
     def change_movement(self, input):
-        if input == 0:
-            GPIO.output(2, GPIO.HIGH)
-            GPIO.output(3, GPIO.LOW)
-        elif input == 2:
-            GPIO.output(2, GPIO.LOW)
-            GPIO.output(3, GPIO.HIGH)
-        elif input == 1 or input == 3:
-            GPIO.output(2, GPIO.LOW)
-            GPIO.output(3, GPIO.LOW)
-        elif input == 4:
-            GPIO.output(4, GPIO.LOW)
-            GPIO.output(17, GPIO.LOW)
-        elif input == 5:
-            GPIO.output(4, GPIO.HIGH)
-            GPIO.output(17, GPIO.LOW)
-        elif input == 6:
-            GPIO.output(4, GPIO.LOW)
-            GPIO.output(17, GPIO.HIGH)
+        if input == Direction.FORWARD:
+            GPIO.output(FORWARD_A, GPIO.HIGH)
+            GPIO.output(FORWARD_B, GPIO.LOW)
+        elif input == Direction.REVERSE:
+            GPIO.output(FORWARD_A, GPIO.LOW)
+            GPIO.output(FORWARD_B, GPIO.HIGH)
+        elif input == Direction.STOP:
+            GPIO.output(FORWARD_A, GPIO.LOW)
+            GPIO.output(FORWARD_B, GPIO.LOW)
+        elif input == Direction.CENTER:
+            GPIO.output(STEERING_A, GPIO.LOW)
+            GPIO.output(STEERING_B, GPIO.LOW)
+        elif input == Direction.LEFT:
+            GPIO.output(STEERING_A, GPIO.HIGH)
+            GPIO.output(STEERING_B, GPIO.LOW)
+        elif input == Direction.RIGHT:
+            GPIO.output(STEERING_A, GPIO.LOW)
+            GPIO.output(STEERING_B, GPIO.HIGH)
         print("Got movement post: " +  str(input))
 
 def runServer(server_class=HTTPServer, handler_class=S, port=5000):
